300218c57a877defcfce1c7c77fd040ec33c2941
Bibhuti Sahu. <sbibhuti246@gmail.com>
2025-03-10 07:19:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n,ans;
+        System.out.println("Enter any Number:");
+        Scanner obj = new Scanner(System.in);
+        n = obj.nextInt();
+
+    }
+}
